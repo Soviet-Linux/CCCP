097ca403933d460db8ef5beae59f116dc7f4fa12
PKD667 <paulkronlund@gmail.com>
2022-04-01 18:35:19 +0200

    it does not work but there is a little progress

--- CCCP/src/main.cpp
+++ CCCP/src/main.cpp
@@ -2,10 +2,24 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 
 std::string CURRENT_DIR = "/home/paulk/Desktop/Soviet/CCCP/";
 std::string BUILD_ROOT = CURRENT_DIR + "build-root/";
+
+int install_package (std::string PName);
+int install_binary ();
+std::vector<std::string> open_spm (std::string PName);
+std::vector<std::string> init_pkg_list ();
+
+std::vector<std::string> installed_packages = init_pkg_list ();
+
+int main () {
+
+  install_package("lolcat");
+
+  return 0;
+}
 
 //Getting package data from file
 std::vector<std::string> open_spm (std::string PName)
@@ -41,15 +55,44 @@ int install_binary ()
 {
 
 }
+
+std::vector<std::string> init_pkg_list ()
+{
+
+    std::streampos size;
+    char * memblock;
+
+    std::vector<std::string> pkg_list;
+
+    std::ifstream list_file ( CURRENT_DIR + "pkg.list", std::ios::in);
+    std::string line;
+    if (list_file.is_open())
+    {
+        std::cout << "file opened " << "\n";
+        while ( getline (list_file,line) )
+        {
+            pkg_list.push_back(line);
+        }
+        list_file.close();
+    }
+    else std::cout << "ITS BAD"<< "\n";
+
+    return pkg_list;
+}
+
 //parsing data and installing package
 int install_package (std::string PName)
 {
     std::cout << "processing package" << "\n";
     std::vector<std::string> pkg_info = open_spm("lolcat");
     std::cout << "package info parsed" << "\n";
-    if ( pkg_info[0] != "nothing")
+    if ( pkg_info[0] != "")
     {
-    //Dependencies stuff for LATER
+        for (int i = 1; i < pkg_info[0];i++)
+        {
+            if(std::find(installed_packages.begin(), installed_packages.end(), ) != installed_packages.end())   
+        }
+        
     }
     else {
         std::cout << "No dependencies" << "\n";
@@ -71,9 +114,18 @@ int install_package (std::string PName)
     }
 }
 
-int main () {
-
-  install_package("lolcat");
-
-  return 0;
+std::vector<std::string> split(std::string str, std::string token){
+    std::vector<std::string>result;
+    while(str.size()){
+        int index = str.find(token);
+        if(index!=std::string::npos){
+            result.push_back(str.substr(0,index));
+            str = str.substr(index+token.size());
+            if(str.size()==0)result.push_back(str);
+        }else{
+            result.push_back(str);
+            str = "";
+        }
+    }
+    return result;
 }
