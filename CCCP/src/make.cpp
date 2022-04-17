0ebb6176f63b28b34161c4c9605388589b01d453
PKD667 <paulkronlund@gmail.com>
2022-04-17 16:02:18 +0200

    some things i dont rly know what

--- CCCP/src/make.cpp
+++ CCCP/src/make.cpp
@@ -98,4 +98,34 @@ void bin_spm (const std::string& in_path , const std::string& out_path)
     file_spm_bin.close();
     
 }
+//This fucntion is very important , it will store the install location data to the "DB"
+void store_spm (const std::string& PPath, const std::string& out_path,const std::string& data_file_path)
+{
+    std::ifstream file_spm((PPath).c_str(), std::ios::in);
+    std::stringstream buffer;
+    buffer << file_spm.rdbuf();
+    file_spm.close();
+    //parsing json data
+    auto pkg_info = json::parse(buffer.str());
+    //Get package file loaction
+    //this work is a little outside the scope of this function , but its ok
+    // TODO: change this // This is changing rn
+    //also the /tmp file is alittle hacky i think
+    //Add the package locations
+    std::string line;
+    std::ifstream data_file (data_file_path.c_str());
+    if (data_file.is_open())
+    {
+        while ( getline (data_file,line) )
+        {
+            std::cout << line << '\n';
+            pkg_info["locations"].push_back(line);
+        }
+        data_file.close();
+    }
+    
+    std::ofstream file_spm_out((out_path).c_str(), std::ios::out);
+    file_spm_out << pkg_info.dump(4);
+    file_spm_out.close();
+}
 
