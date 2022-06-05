243a5d68e11d5ebb3eecb0cd27ddbc22495803cb
PKD <pkd@sovietlinux.ml>
2022-06-05 19:43:27 +0200

    Re-adding makefile for compatibility reasons

--- make.sh
+++ make.sh
@@ -14,42 +14,61 @@ CXXFLAGS="$CFLAGS -std=c++17"
 LIB_SRC_DIR=src/libspm
 CCCP_SRC_DIR=src/cccp
 
-LIBSPM_DIR=bin
+LIBSPM_DIR=$BIN_DIR
+
+
+function libspm()
+{
+    echo "Building lib..."
+    SRCS=$(find $LIB_SRC_DIR -name *.cpp)
+    echo $SRCS
+    for FILE in $SRCS; do
+        temp=$(echo $FILE | sed "s/\.cpp/.o/")
+        OBJ_FILE="${temp/$LIB_SRC_DIR/$OBJ_DIR}"
+        echo "Compiling $FILE... to $OBJ_FILE"
+        $CXX $CXXFLAGS -c -o $OBJ_FILE $FILE -fPIC
+        OBJS="$OBJS $OBJ_FILE"
+    done
+    echo "Linking lib..."
+
+    $CXX -shared -o $BIN_DIR/libspm.so $OBJS
+    echo "Done."
+}
+function cccp()
+{
+    echo "Building cccp..."
+    SRCS=$(find $CCCP_SRC_DIR -name *.cpp)
+    echo $SRCS
+    for FILE in $SRCS; do
+        echo $FILE
+        temp=$(echo $FILE | sed "s/\.cpp/.o/")
+        OBJ_FILE="${temp/$CCCP_SRC_DIR/$OBJ_DIR}"
+        echo "Compiling $FILE... to $OBJ_FILE"
+        $CXX  $CXXFLAGS -c -o $OBJ_FILE $FILE 
+        OBJS="$OBJS $OBJ_FILE"
+    done
+    echo "Linking cccp..."
+    echo " $CXX $CXXFLAGS -o $BIN_DIR/cccp $OBJS -fPIC -lspm"
+    $CXX -L$LIBSPM_DIR $CXXFLAGS -o $BIN_DIR/cccp $OBJS -fPIC -lspm
+    echo "Done."
+}
+
+# Checking if args are given
 if [ -z "$1" ]; then
     echo "Usage: ./make.sh [lib|cccp]"
+    exit 1
 else
     if [ $1 = "lib" ]; then
-        echo "Building lib..."
-        SRCS=$(find $LIB_SRC_DIR -name *.cpp)
-        echo $SRCS
-        for FILE in $SRCS; do
-            temp=$(echo $FILE | sed "s/\.cpp/.o/")
-            OBJ_FILE="${temp/$LIB_SRC_DIR/$OBJ_DIR}"
-            echo "Compiling $FILE... to $OBJ_FILE"
-            $CXX $CXXFLAGS -c -o $OBJ_FILE $FILE -fPIC
-            OBJS="$OBJS $OBJ_FILE"
-        done
-        echo "Linking lib..."
-
-        $CXX -shared -o $BIN_DIR/libspm.so $OBJS
-        echo "Done."
+        libspm
     elif [ $1 = "cccp" ]; then
-        echo "Building cccp..."
-        SRCS=$(find $CCCP_SRC_DIR -name *.cpp)
-        echo $SRCS
-        for FILE in $SRCS; do
-            echo $FILE
-            temp=$(echo $FILE | sed "s/\.cpp/.o/")
-            OBJ_FILE="${temp/$CCCP_SRC_DIR/$OBJ_DIR}"
-            echo "Compiling $FILE... to $OBJ_FILE"
-            $CXX  $CXXFLAGS -c -o $OBJ_FILE $FILE 
-            OBJS="$OBJS $OBJ_FILE"
-        done
-        echo "Linking cccp..."
-        echo " $CXX $CXXFLAGS -o $BIN_DIR/cccp $OBJS -fPIC -lspm"
-        $CXX -L$LIBSPM_DIR $CXXFLAGS -o $BIN_DIR/cccp $OBJS -fPIC -lspm
-        echo "Done."
+        cccp
     else
-    echo "Usage: ./make.sh [lib|cccp]"
+        echo "Usage: ./make.sh [lib|cccp]"
+        exit 1
     fi
 fi
+
+
+
+
+
---
+++ makefile
@@ -0,0 +1,8 @@
+
+all:
+	./make.sh lib
+	./make.sh cccp
+libspm:
+	./make.sh lib
+cccp:
+	./make.sh cccp
