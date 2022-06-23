55eba3582e92f7b4cbf698d739b26d760a3793c6
PKD <pkd@sovietlinux.ml>
2022-06-23 18:26:17 +0200

    colors !!!!!

--- src/libspm/debug.cpp
+++ src/libspm/debug.cpp
@@ -6,7 +6,23 @@
 // class stuff
 #include "../../include/libspm.hpp"
 
-
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 int soviet::msg(level msgLevel, const std::string& message,...)
 {
@@ -29,19 +45,19 @@ int soviet::msg(level msgLevel, const std::string& message,...)
     switch (msgLevel)
     {
         case level::DBG:
-            if (DEBUG) std::cout << "DEBUG: " << FmtMsg << std::endl;
+            if (DEBUG) std::cout << BOLDGREEN << " DEBUG: " << RESET << GREEN << FmtMsg << RESET << std::endl;
             break;
         case level::INFO:
-            std::cout << "INFO: " << FmtMsg << std::endl;
+            std::cout << BOLDBLUE << "INFO: " << RESET  << BLUE << FmtMsg << RESET << std::endl;
             break;
         case level::WARNING:
-            std::cout << "WARNING: " << FmtMsg << std::endl;
+            std::cout << BOLDYELLOW << "WARNING: " << RESET  << YELLOW << FmtMsg << RESET << std::endl;
             break;
         case level::ERROR:
-            std::cout << "ERROR: " << FmtMsg << std::endl;
+            std::cout << BOLDMAGENTA << "ERROR: " << RESET  << MAGENTA << FmtMsg << RESET << std::endl;
             break;
         case level::FATAL:
-            std::cout << "FATAL: " << FmtMsg << std::endl;
+            std::cout << BOLDRED << "FATAL: " << RESET  << RED << FmtMsg <<  RESET << std::endl;
             exit(1);
         default:
             std::cout << "UNKNOWN: " << FmtMsg << std::endl;
