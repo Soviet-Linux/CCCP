b029d605e7cb3b20e6831e9ff9c769a174b7d4bb
PKD <pkd@sovietlinux.ml>
2022-08-01 15:45:09 +0200

    fixing makefile

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ CXX=g++
 CCCP_SRC_DIR=src/cccp
 
 OBJ_DIR=obj
-BIN_DIR=./bin
+BIN_DIR=bin
 
 CFLAGS="-Wall -Wextra  -pedantic -O2   "
 CXXFLAGS="$CFLAGS -std=c++17"
@@ -17,7 +17,8 @@ CCCP_SRC_DIR=src/cccp
 
 LIBSPM_DIR=$BIN_DIR
 
-REPO_STR="REPOS=http://our.sovietlinux.ml"
+
+
 
 # This is the path to a soviet chroot
 #SOVIET=soviet
@@ -25,8 +26,6 @@ REPO_STR="REPOS=http://our.sovietlinux.ml"
 
 function libspm()
 {
-    mkdir $OBJ_DIR
-    mkdir $BIN_DIR
     echo "Building lib..."
     LIB_SRCS=$(find $LIB_SRC_DIR -name *.cpp)
     echo $LIB_SRCS
@@ -75,30 +74,9 @@ else
         libspm
     elif [ $1 = "cccp" ]; then
         cccp
-    elif [ $1 = "all" ]; then
-        libspm
-        cccp
     elif [ $1 = "clean" ]; then
         rm -rf $OBJ_DIR/*
         rm -rf $BIN_DIR/*
-    elif [ $1 = "chroot" ]; then
-        cp $BIN_DIR/libspm.so $SOVIET/usr/lib/libspm.so
-        cp $BIN_DIR/cccp $SOVIET/usr/bin/cccp
-        if  grep -q $REPO_STR $SOVIET/etc/cccp.conf ; then
-            echo "Repo already in cccp.conf"
-        else 
-            echo $REPO_STR >> $SOVIET/etc/cccp.conf
-        fi
-    elif [ $1 = "install" ]; then
-        cp $BIN_DIR/libspm.so /usr/lib/libspm.so
-        cp $BIN_DIR/cccp /usr/bin/cccp
-        cp scripts/* /usr/bin
-	    if  grep -q $REPO_STR "/etc/cccp.conf" ; then
-            echo "Repo already in /etc/cccp.conf"
-        else 
-            echo $REPO_STR >> $SOVIET/etc/cccp.conf
-        fi
-
     elif [ $1 = "uninstall" ]; then
         rm /usr/lib/libspm.so
         rm /usr/bin/cccp
--- makefile
+++ makefile
@@ -1,17 +1,22 @@
 
 all:
-	./make.sh all
+	mkdir bin
+	mkdir obj
+	./make.sh lib
+	./make.sh cccp
 libspm:
 	./make.sh lib
 cccp:
 	./make.sh cccp
 install:
-	./make.sh install
+	cp bin/libspm.so $(DESTDIR)/usr/lib/libspm.so
+	cp bin/cccp $(DESTDIR)/usr/bin/cccp
+	cp scripts/* $(DESTDIR)/usr/bin
+	echo REPOS=http://our.sovietlinux.ml > $(DESTDIR)/etc/cccp.conf
+
 uninstal:
 	./make.sh uninstall
 test:
 	./make.sh test
-chroot:
-	./make.sh chroot
 clean:
 	./make.sh clean
