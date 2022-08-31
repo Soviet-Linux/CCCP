5614726c41e40619fcc36e1e514d2abdb4b5ec3e
Okko <okko.oikkonen@gmail.com>
2022-08-31 21:21:04 +0300

    make version info more clean (and use const when possible)

--- src/cccp/rust/src/main.rs
+++ src/cccp/rust/src/main.rs
@@ -2,16 +2,17 @@ use libspm_rs::*;
 
 use clap::{Arg, ArgAction, Command};
 
+const FRONT_END_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let VERSION: String = format!("\nRust front-end version: {}\nlibspm-rs version: {}",
-        env!("CARGO_PKG_VERSION").to_owned(),
+    let version: String = format!("\nRust front-end version: {}\nlibspm-rs version: {}",
+        FRONT_END_VERSION,
         libspm_rs::get_version().to_string()
     ); // this implementation is ugly, but couldn't get it to work as a const
 
     let matches = Command::new("cccp")
         .about("CCCP Crafter of Controlled Packages")
-        .version(&VERSION[..])
+        .version(&*version)
         .subcommand_required(true)
         .arg_required_else_help(true) // if no args are supplied, then print help
         .author("https://git.sovietlinux.ml/sovietlinux/CCCP/")
