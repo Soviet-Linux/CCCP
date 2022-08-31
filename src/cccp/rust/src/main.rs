8af661c68d50e25cfaa4c445272559030dcaa421
Okko <okko.oikkonen@gmail.com>
2022-08-31 22:16:19 +0300

    most vital commands added, pretty repetitive, this could probably be made way better

--- src/cccp/rust/src/main.rs
+++ src/cccp/rust/src/main.rs
@@ -20,10 +20,53 @@ fn main() {
             Command::new("install") // install
                 .short_flag('i') // -i 
                 .long_flag("install") // --install
-                .about("Install a package from the repos")
+                .about("Install a package from the OUR")
+                .arg_required_else_help(true)
                 .arg(
                     Arg::new("package")
                         .help("the package names you want to install")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .multiple_values(true)
+                )
+        )
+        .subcommand(
+            Command::new("package") // package
+                .short_flag('p') // -p 
+                .long_flag("package") // --package
+                .about("Install a package file")
+                .arg_required_else_help(true)
+                .arg(
+                    Arg::new("package")
+                        .help("The package file you want to install")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .multiple_values(true)
+                )
+        )
+        .subcommand(
+            Command::new("remove") // remove
+                .short_flag('r') // -p 
+                .long_flag("remove") // --remove
+                .about("Uninstall a package")
+                .arg_required_else_help(true)
+                .arg(
+                    Arg::new("package")
+                        .help("The name of the package you want to uninstall")
+                        .required(true)
+                        .action(ArgAction::Set)
+                        .multiple_values(true)
+                )
+        )
+        .subcommand(
+            Command::new("check") // check
+                .short_flag('c') // -p 
+                .long_flag("check") // --check
+                .about("Check if a package is insalled")
+                .arg_required_else_help(true)
+                .arg(
+                    Arg::new("package")
+                        .help("The name of the package you want to check")
                         .required(true)
                         .action(ArgAction::Set)
                         .multiple_values(true)
@@ -41,7 +84,31 @@ fn main() {
             // this ^ packages vector would be used probably for the function calls
             let values = packages.join(", "); // a comma separated string with each package name 
             println!("Installing {}...", values);
-        }
+        },
+        Some(("package", package_mathces)) => {
+            let packagefiles: Vec<_> = package_mathces
+                .get_many::<String>("package")
+                .expect("is present")
+                .map(|s| s.as_str())
+                .collect();
+            println!("Installing package files {}", packagefiles.join(", "));
+        },
+        Some(("remove", remove_matches)) => {
+            let packages: Vec<_> = remove_matches
+                .get_many::<String>("package")
+                .expect("is present")
+                .map(|s| s.as_str())
+                .collect();
+            println!("Uninstalling {} ...", packages.join(", "));
+        },
+        Some(("check", remove_matches)) => {
+            let packages: Vec<_> = remove_matches
+                .get_many::<String>("package")
+                .expect("is present")
+                .map(|s| s.as_str())
+                .collect();
+            println!("checking if {} is installed...", packages.join(", "));
+        },
         _ => {
             unreachable!()
         }
