1b9127895309cfa1103bbae486751b37dbf8b309
TheHolyTachanka <TheHolyTachankaYT@proton.me>
2022-11-21 14:36:08 +0200

    moved libspm to saparate repo

--- src/formats/ecmp/ecmp.h
+++
@@ -1,6 +0,0 @@
-
-struct ecmp_section {
-    char *name;
-    char *buff; 
-};
-unsigned int ecmp_parse_file(struct ecmp_section*** sections,char* path);
--- src/formats/spm/json.h
+++
@@ -1,6 +0,0 @@
-#include "jsmn.h"
-
-char* jstrval (jsmntok_t t,char* jstr);
-char** jarrtoarr (jsmntok_t t_list[],char* jstr,int pos);
-char* arrtojarr (char** arr,int count);
-unsigned long jparse(jsmntok_t** t,char* jstr,unsigned long jsize);
