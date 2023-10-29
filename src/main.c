7233d78d8b9f765e0f1a6fe27154233126a7010a
JohnVictoryz <john.g.zioupos@gmail.com>
2023-10-29 17:47:08 +0200

    Added -v and -h commands and added fastest compiler flag ```-Ofast```

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include </usr/include/spm/libspm.h>
 #include </usr/include/spm/cutils.h>
@@ -8,7 +9,7 @@
 #include </usr/include/spm/globals.h>
 
 
-#define C_FRONTEND_VERSION 0.1
+const float C_FRONTEND_VERSION = 0.1;
 
 
 
@@ -28,6 +29,10 @@ char* ART = "\033[31;1;5m"
     "⠀⠀⠀⠑⠯⣭⣼⡷⠤⣄⡠⢄⣠⢤⣀⡤⠾⣯⣭⡽⠊⠀⠀⠀\n"
     "⠀⠀⠀⠀⠀⠑⠒⠚⠛⣩⠞⠁⠀⠙⢮⠛⠛⠒⠊⠀⠀⠀⠀⠀\n"
     "\033[0m";
+
+char* HELP = "\x1b[34m Usage cccp [options/package] \x1b[0m \n"
+    "\x1b[32m Options: 1) -v Displays the version \n"
+    "          2) -h Displays this message    \x1b[0m \n";
 
 int _print_version_(unsigned int* i);
 
@@ -56,23 +61,34 @@ void* args[][2] = {
 
 char** ARGV;
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
+
+    
 
     dbg(2,"DEBUG Enabled!");
 
-    if (argc < 2) {
-        msg(ERROR, "No command specified");
+    if (argc == 1) {
+       msg(ERROR, "No command specified add -h for information about how the command should be used.");
         return 1;
     }
-    // root check
-    if (getuid() != 0) {
-        msg(ERROR, "You need to be root to run this program");
+    if (0 == strcmp(argv[1], "-h")) {
+        printf(HELP);
+        return 0;
+    }
+    // root check and --help and --version commands 
+    if (getuid() != 0 && 0 != strcmp(argv[1], "-h"), 0 != strcmp(argv[1], "-v")) {
+        msg(ERROR, "You need to be root to run this program for help type -h");
         return 1;
+    } else if (0 == strcmp(argv[1], "-v")) {
+        printf(ART,C_FRONTEND_VERSION,version());
+        return 0;
+    } else {
+        dbg(2,"Running as root");
     }
 
     hashtable* hm = hm_init(args, sizeof(args)/sizeof(args[0]));
 
-    ARGV = argv;
+  
 
     init(); 
     int (*func)(int*);
